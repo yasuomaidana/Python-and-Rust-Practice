bddbafc119071d37d576b14d317745bc1108a3ad
yasuomaidana <yasuomaidana@gmail.com>
2025-02-18 15:36:33 -0600

    COUR-1856: Using a proper package and adding error handling

--- blkpy/main.py
+++ blkpy/main.py
@@ -1,3 +1,5 @@
+from subprocess import CalledProcessError
+
 import click
 from lsblk_selector import run_lsblk
 
@@ -8,4 +10,16 @@ from lsblk_selector import run_lsblk
 def main(device, verbose):
     print(f"Device: {device}")
     print(f"Verbose: {verbose}")
-    print(f"{run_lsblk(device)}")
+    try:
+        print(f"{run_lsblk(device)}")
+    except CalledProcessError as e:
+        print(f"Sorry couldn't run the code because of: \n\t{e}")
+    except FileNotFoundError as e:
+        if "lsblk" in str(e):
+            click.echo(click.style(
+                f"Sorry, couldn't find the lsblk command. Make sure it is installed 😒.",
+                fg='red'))
+        else:
+            print(f"Sorry, couldn't find the file: \n\t{e}")
+    except Exception as e:
+        print(f"Sorry, something went wrong. IDK and IDC.{e}, type: {type(e)}")
