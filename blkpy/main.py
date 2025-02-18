6fb364f683b316920149972581f63f45b1a704c2
yasuomaidana <yasuomaidana@gmail.com>
2025-02-18 11:08:54 -0600

    COUR-1856: Creating python cli tool

---
+++ blkpy/main.py
@@ -0,0 +1,11 @@
+import click
+from lsblk_selector import run_lsblk
+
+
+@click.command()
+@click.option('--verbose', '-v', is_flag=True)
+@click.argument('device')
+def main(device, verbose):
+    print(f"Device: {device}")
+    print(f"Verbose: {verbose}")
+    print(f"{run_lsblk(device)}")
