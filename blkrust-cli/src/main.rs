1f30f03865011a9fabb2e185a303f087e74ae0e9
yasuomaidana <yasuomaidana@gmail.com>
2025-02-20 10:52:44 -0600

    COUR-1856: Running program

--- blkrust-cli/src/main.rs
+++ blkrust-cli/src/main.rs
@@ -1,4 +1,17 @@
+mod command_runner;
+mod lsblk;
+
 fn main() {
-    println!("Hello, world!");
-    println!("Operating system: {}", std::env::consts::OS);
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} DEVICE", args[0]);
+        std::process::exit(1);
+    }
+    println!("{:?}", args);
+    let device = &args[1];
+    println!("Device: {}", device);
+    match lsblk::run_lsblk(device) {
+        Ok(device) => println!("{}", device),
+        Err(error) => eprintln!("{}", error),
+    }
 }
