b22b8f053df1d07ab4508ed21ca722aea0d0db83
yasuomaidana <yasuomaidana@gmail.com>
2025-02-20 12:33:56 -0600

    COUR-1856: Using clap to read the cli arguments

--- blkrust-cli/src/main.rs
+++ blkrust-cli/src/main.rs
@@ -1,14 +1,12 @@
+use clap::Parser;
+
+mod args;
 mod command_runner;
 mod lsblk;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Usage: {} DEVICE", args[0]);
-        std::process::exit(1);
-    }
-    println!("{:?}", args);
-    let device = &args[1];
+    let args = args::Args::parse();
+    let device = &args.device;
     println!("Device: {}", device);
     match lsblk::run_lsblk(device) {
         Ok(device) => println!("{}", device),
