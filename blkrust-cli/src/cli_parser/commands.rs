2a4dbdca2afa55b98763d0ea8a82037520fe8d0a
yasuomaidana <yasuomaidana@gmail.com>
2025-02-20 22:22:00 -0600

    COUR-1857: Adding default value to variables loaded from environmental variables and changing debug to users input

--- blkrust-cli/src/cli_parser/commands.rs
+++ blkrust-cli/src/cli_parser/commands.rs
@@ -4,9 +4,8 @@ use clap::{ArgAction, Parser};
 #[derive(Parser, Debug)]
 #[command(author = "Yo mero",version, about="A rust lsbk tool", long_about = None)]
 pub struct Opts {
-    // #[clap(short, long, help = "Prints debug information", default_value_t = false)]
-    // pub debug: bool,
-    #[clap(short, long, help = "Prints debug information", env = "BLKRS_DEBUG")]
+    #[clap(short, long, help = "Prints debug information", default_value_t = false)]
+    // #[clap(short, long, help = "Prints debug information", env = "BLKRS_DEBUG", default_value = "true")]
     pub debug: bool,
 
     #[clap(short, long, action = ArgAction::Count, help = "Defines the level of verbosity"
