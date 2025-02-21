13752d16dc93bc21021a969a6975bdecb5953228
yasuomaidana <yasuomaidana@gmail.com>
2025-02-20 22:21:17 -0600

    COUR-1857: Using enums to limit users input

--- blkrust-cli/src/cli_parser/command.rs
+++ blkrust-cli/src/cli_parser/command.rs
@@ -1,8 +1,21 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Parser, Debug)]
 pub struct InfoOpts {
     // #[clap(short, long, help = "The device to get info about")]
     #[clap(help = "The device to get info about")]
     pub device: String,
+}
+
+#[derive(Parser, Debug)]
+pub struct TimeOpts {
+    #[clap(short, long, help = "Prints debug information", env = "TIME_FMT"
+    , default_value = "human")]
+    pub format: TimeFormat,
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+pub enum TimeFormat {
+    Human,
+    Unix,
 }
