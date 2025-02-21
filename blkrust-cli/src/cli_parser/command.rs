cb7190fa76ff2297c5c9cda6999b52d32fc436a1
yasuomaidana <yasuomaidana@gmail.com>
2025-02-21 11:19:58 -0600

    COUR-1857: Changing default time format value and using enum values as cli input

--- blkrust-cli/src/cli_parser/command.rs
+++ blkrust-cli/src/cli_parser/command.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, ValueEnum};
+use std::fmt::Display;
 
 #[derive(Parser, Debug)]
 pub struct InfoOpts {
@@ -9,13 +10,32 @@ pub struct InfoOpts {
 
 #[derive(Parser, Debug)]
 pub struct TimeOpts {
-    #[clap(short, long, help = "Prints debug information", env = "TIME_FMT"
-    , default_value = "human")]
+    #[clap(
+        short,
+        long,
+        help = "Prints debug information",
+        env = "TIME_FMT",
+        default_value = "default"
+    )]
     pub format: TimeFormat,
 }
 
 #[derive(Debug, Clone, ValueEnum)]
 pub enum TimeFormat {
-    Human,
-    Unix,
+    RFC3339,
+    Default,
+}
+
+impl Display for TimeFormat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let time = chrono::Local::now();
+        match self {
+            TimeFormat::RFC3339 => {
+                write!(f, "{}", time.to_rfc3339())
+            }
+            TimeFormat::Default => {
+                write!(f, "{}", time.format("%Y-%m-%d %H:%M:%S").to_string())
+            }
+        }
+    }
 }
