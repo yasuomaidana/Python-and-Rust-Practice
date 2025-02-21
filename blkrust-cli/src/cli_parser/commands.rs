5c2f663c198bcbb7655308d7c2b6d5c5320e1b21
yasuomaidana <yasuomaidana@gmail.com>
2025-02-21 11:23:45 -0600

    COUR-1857: Implementing command running at enum level

--- blkrust-cli/src/cli_parser/commands.rs
+++ blkrust-cli/src/cli_parser/commands.rs
@@ -1,5 +1,6 @@
 use crate::cli_parser::command::{InfoOpts, TimeOpts};
 use clap::{ArgAction, Parser};
+use crate::lsblk;
 
 #[derive(Parser, Debug)]
 #[command(author = "Yo mero",version, about="A rust lsbk tool", long_about = None)]
@@ -24,4 +25,22 @@ pub(crate) enum Command {
     Info(InfoOpts),
     #[clap(name = "time", about = "Get the current time")]
     Time(TimeOpts),
+}
+
+impl Command {
+    pub fn run(&self) {
+        match self {
+            Command::Info(info) => {
+                let device = &info.device;
+                println!("Device: {}", device);
+                match lsblk::run_lsblk(device) {
+                    Ok(device) => println!("{}", device),
+                    Err(error) => eprintln!("{}", error),
+                }
+            }
+            Command::Time(time) => {
+                println!("Now is: {}", time.format);
+            }
+        }
+    }
 }
