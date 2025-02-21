d715adc5b121e6d7a0d66f0b1c4be2d35b955d65
yasuomaidana <yasuomaidana@gmail.com>
2025-02-20 21:53:42 -0600

    COUR-1857: Changing debug parameter as an environmental one

--- blkrust-cli/src/cli_parser/commands.rs
+++ blkrust-cli/src/cli_parser/commands.rs
@@ -1,19 +1,20 @@
 use crate::cli_parser::command::InfoOpts;
-use clap::{Parser, ArgAction};
+use clap::{ArgAction, Parser};
 
 #[derive(Parser, Debug)]
 #[command(author = "Yo mero",version, about="A rust lsbk tool", long_about = None)]
 pub struct Opts {
-    
-    #[clap(short, long, help = "Prints debug information", default_value_t = false)]
+    // #[clap(short, long, help = "Prints debug information", default_value_t = false)]
+    // pub debug: bool,
+    #[clap(short, long, help = "Prints debug information", env = "BLKRS_DEBUG")]
     pub debug: bool,
-    
+
     #[clap(short, long, action = ArgAction::Count, help = "Defines the level of verbosity"
     )]
     // default value without defining it is zero
     // , default_value = "1")]
     pub verbose_level: u8,
-    
+
     #[clap(subcommand)]
     pub cmd: Command,
 }
--- blkrust-cli/src/main.rs
+++ blkrust-cli/src/main.rs
@@ -7,16 +7,17 @@ mod lsblk;
 
 fn main() {
     let args = Opts::parse();
+
     println!("Debug: {}", args.debug);
-    
-    match args.verbose_level { 
+
+    match args.verbose_level {
         0 => println!("Verbose level: None"),
         1 => println!("Verbose level: Low"),
         2 => println!("Verbose level: Medium"),
         3 => println!("Verbose level: High"),
         _ => println!("Verbose level: Too much"),
     }
-    
+
     match args.cmd {
         Command::Info(info) => {
             let device = &info.device;
