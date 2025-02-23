b41218709c82542a09495e90e5edb6ff56ce0210
yasuomaidana <yasuomaidana@gmail.com>
2025-02-22 23:31:21 -0600

    COUR-1857: Adding a format to a log

--- blkpy-cli/blkpy/main.py
+++ blkpy-cli/blkpy/main.py
@@ -1,9 +1,29 @@
 import click
-
-from .commands import info, nice, show_time, print_file
+import datetime
+import logging
+from blkpy.commands import info, nice, show_time, print_file
+from blkpy.log_formatter import CustomFormatter
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+# log = logging.getLogger(__name__)
+log = logging.getLogger("cli-root")
 
+# Define format for logs
+fmt = '%(asctime)s | %(levelname)8s | %(message)s'
+
+# Create stdout handler for logging to the console (logs all five levels)
+stdout_handler = logging.StreamHandler()
+stdout_handler.setFormatter(CustomFormatter(fmt))
+
+# Create file handler for logging to a file (logs all five levels)
+today = datetime.date.today()
+file_handler = logging.FileHandler('my_app_{}.log'.format(today.strftime('%Y_%m_%d')))
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter(fmt))
+
+# Add both handlers to the logger
+log.addHandler(stdout_handler)
+log.addHandler(file_handler)
 
 # context settings also works with command
 @click.group(
@@ -15,13 +35,22 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option('--debug-level', '-d',
               envvar='DEBUG_LEVEL',
               type=click.IntRange(0, 3), help="Set debug level optionally with env var DEBUG_LEVEL"
-              , default=0)
+    , default=0)
 @click.pass_context
 def main(ctx, verbose, debug_level):
     ctx.ensure_object(dict)
     ctx.obj.update(locals())
-    print(f"Debug level: {debug_level}")
-    pass
+
+    if debug_level == 0:
+        logging.basicConfig(level=logging.DEBUG)
+    elif debug_level == 1:
+        logging.basicConfig(level=logging.INFO)
+    elif debug_level == 2:
+        logging.basicConfig(level=logging.WARNING)
+    elif debug_level == 3:
+        logging.basicConfig(level=logging.ERROR)
+    if verbose:
+        log.debug("Verbose mode enabled")
 
 
 ## Long way to do it
@@ -37,3 +66,6 @@ main.add_command(info)
 main.add_command(nice)
 main.add_command(show_time)
 main.add_command(print_file)
+
+if __name__ == '__main__':
+    main()
