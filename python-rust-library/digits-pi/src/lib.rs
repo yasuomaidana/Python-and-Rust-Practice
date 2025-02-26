296f1c2c59ebb5a64933a3a07ed18440af9a23b9
yasuomaidana <yasuomaidana@gmail.com>
2025-02-25 21:01:38 -0600

    COUR-1858: Getting Pi using Leibniz with Shank's transformation summations

--- python-rust-library/digits-pi/src/lib.rs
+++ python-rust-library/digits-pi/src/lib.rs
@@ -3,14 +3,15 @@ use pyo3::{pyfunction, wrap_pyfunction, PyResult, Python};
 use rayon::iter::IntoParallelIterator;
 use rayon::iter::ParallelIterator;
 
-/// Calculate the value of π using the Leibniz formula.
+/// Calculate the value of π using the Leibniz with  Shanks transformation formula.
 #[pyfunction]
 fn calculate_pi(iterations: u32) -> PyResult<f64> {
     let pi = (0..iterations)
         .into_par_iter()
         .map(|i| {
             let i = i as f64;
-            (-1_f64).powf(i) / (2.0 * i + 1.0)
+            let den = (4.0 * i + 1.0) * (4.0 * i + 3.0);
+            2.0 / den
         })
         .sum::<f64>()
         * 4.0;
@@ -29,7 +30,7 @@ mod tests {
 
     #[test]
     fn test_calculate_pi() {
-        let result = calculate_pi(90).unwrap();
+        let result = calculate_pi(44).unwrap();
         let expected = 3.14; // Approximate value of π
         assert!((result - expected).abs() < 0.01);
     }
