10cb6aa719053f6bd99e725399dc52698d492d59
yasuomaidana <yasuomaidana@gmail.com>
2025-02-27 14:31:32 -0600

    COUR-1858: Initializing a thread free python context

--- python-rust-library/py-rs-call/src/main.rs
+++ python-rust-library/py-rs-call/src/main.rs
@@ -1,15 +1,18 @@
-use pyo3::ffi::Py_Initialize;
 use pyo3::prelude::*;
+use pyo3::prepare_freethreaded_python;
 
 fn main() -> PyResult<()> {
-    unsafe { Py_Initialize(); }
+    prepare_freethreaded_python();
     Python::with_gil(|py| {
         let builtins = PyModule::import(py, "builtins")?;
         let total: i32 = builtins
             .getattr("sum")?
             .call1((vec![1, 2, 3],))?
             .extract()?;
-        let _matplotlib = PyModule::import(py, "matplotlib")?;
+        let sys = PyModule::import(py, "sys")?;
+        let python_path:Vec<String> = sys.getattr("path")?.extract()?;
+        println!("Python path: {:?}", python_path);
+        // let _matplotlib = PyModule::import(py, "matplotlib")?;
         println!("Total: {}", total);
         Ok(())
     })
