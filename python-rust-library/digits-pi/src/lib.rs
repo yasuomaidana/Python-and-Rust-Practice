a0f336b52f820ae9b4cf9496bd818d4e6da87ac5
yasuomaidana <yasuomaidana@gmail.com>
2025-02-26 20:38:50 -0600

    COUR-1858: Adding a function which returns a dictionary of different types from Rust to Python

--- python-rust-library/digits-pi/src/lib.rs
+++ python-rust-library/digits-pi/src/lib.rs
@@ -1,4 +1,6 @@
+use std::collections::{HashMap, HashSet};
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
 use pyo3::{pyfunction, wrap_pyfunction, PyResult};
 use rayon::iter::IntoParallelIterator;
 use rayon::iter::ParallelIterator;
@@ -18,9 +20,29 @@ fn calculate_pi(iterations: u32) -> PyResult<f64> {
     Ok(pi)
 }
 
+#[pyfunction]
+fn data_types(py: Python<'_>) -> PyResult<PyObject> {
+    let dict = PyDict::new(py);
+    dict.set_item("int", 42)?;
+    dict.set_item("float", 3.14)?;
+    dict.set_item("str", "Hello, World!")?;
+    dict.set_item("bool", true)?;
+    dict.set_item("list", vec![1, 2, 3])?;
+    dict.set_item("tuple", (1, 2, 3))?;
+    dict.set_item("set", vec![1, 2, 3].into_iter().collect::<HashSet<_>>())?;
+    dict.set_item(
+        "dict",
+        vec![(1, "one"), (2, "two")]
+            .into_iter()
+            .collect::<HashMap<_, _>>(),
+    )?;
+    Ok(dict.into())
+}
+
 #[pymodule]
 fn digits_pi(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(calculate_pi, m)?)?;
+    m.add_function(wrap_pyfunction!(data_types, m)?)?;
     Ok(())
 }
 
