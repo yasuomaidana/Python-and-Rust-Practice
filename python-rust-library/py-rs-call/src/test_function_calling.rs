708baa64c368ef5c1036245b92b3a20affe05a6b
yasuomaidana <yasuomaidana@gmail.com>
2025-02-27 15:03:04 -0600

    COUR-1858: Testing single function script

--- python-rust-library/py-rs-call/src/test_function_calling.rs
+++ python-rust-library/py-rs-call/src/test_function_calling.rs
@@ -1,23 +1,33 @@
 #[cfg(test)]
 mod test_function_calling {
-    // use pyo3::prelude::*;
-    // use pyo3::prepare_freethreaded_python;
+    use pyo3::ffi::c_str;
+    use pyo3::prelude::*;
+    use pyo3::prepare_freethreaded_python;
 
     #[test]
     fn test_calling_python_function() {
-        // prepare_freethreaded_python();
-        // Python::with_gil(|py| {
-        //     let builtins = PyModule::import(py, "builtins").unwrap();
-        //     let total: i32 = builtins
-        //         .getattr("sum").unwrap()
-        //         .call1((vec![1, 2, 3],)).unwrap()
-        //         .extract().unwrap();
-        //     let sys = PyModule::import(py, "sys").unwrap();
-        //     let python_path:Vec<String> = sys.getattr("path").unwrap().extract().unwrap();
-        //     println!("Python path: {:?}", python_path);
-        //     // let _matplotlib = PyModule::import(py, "matplotlib")?;
-        //     println!("Total: {}", total);
-        // });
-        assert_eq!(1, 1);
+        prepare_freethreaded_python();
+        let single_function = c_str!(include_str!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/../to_call/single_function.py"
+        )));
+
+        Python::with_gil(|py| {
+            let to_call = PyModule::from_code(
+                py,
+                single_function,
+                c_str!("single_function"),
+                c_str!("to_call"),
+            )
+            .unwrap();
+            let args = (1, 2);
+            let total: i32 = to_call
+                .getattr("simple_sum")
+                .unwrap()
+                .call1(args)
+                .unwrap()
+                .extract()
+                .unwrap();
+            assert_eq!(total, 3);
     }
 }
