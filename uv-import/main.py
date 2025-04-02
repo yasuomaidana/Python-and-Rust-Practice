bf6930165d6b1d894a8986ec19ac75d61e6b1601
yasuomaidana <yasuomaidana@gmail.com>
2025-04-02 09:06:19 -0600

    COUR-1858: Installing project using hatching

--- uv-import/main.py
+++ uv-import/main.py
@@ -1,4 +1,10 @@
-from  uv_lib import custom_sum
-if __name__ == '__main__':
-    print("Wheel installed library")
+from uv_lib import custom_sum
+
+
+def main():
+    print("This is the main function.")
     print(custom_sum(1, 2))
+
+
+if __name__ == '__main__':
+    main()
